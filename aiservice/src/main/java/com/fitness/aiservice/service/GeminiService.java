3842737009b0a44387d3f0f5fffb46e8514bd76f
sajal327 <sajallaha16@gmail.com>
2025-12-20 23:56:12 +0530

    web client added

--- aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -1,4 +1,12 @@
 package com.fitness.aiservice.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
 public class GeminiService {
+    private final WebClient webClient;
+
 }
